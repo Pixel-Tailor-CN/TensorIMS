c74f6320a284fb362d5c65d53f042c588fe1022a
Mystery0 M <mystery0dyl520@gmail.com>
2026-09-16 11:49:30 +0800

    界面：定义设置中心导航路由

---
+++ app/src/main/java/io/github/vvb2060/ims/ui/navigation/TensorImsRoutes.kt
@@ -0,0 +1,9 @@
+package io.github.vvb2060.ims.ui.navigation
+
+object TensorImsRoutes {
+    const val HOME = "home"
+    const val IMS_CONFIG = "ims_config"
+    const val SYSTEM_NETWORK = "system_network"
+    const val CAPTIVE_PORTAL = "captive_portal"
+    const val ADVANCED_TOOLS = "advanced_tools"
+}
